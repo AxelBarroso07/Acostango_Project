41b9e9ba17b96db3be1ffed6e0e2e14403b4f99f
ArielBritezDiaz <arielbritezdiaz@gmail.com>
2024-01-19 09:19:29 -0300

    Test db and backend

--- src/server/controllers/index.controller.js
+++ src/server/controllers/index.controller.js
@@ -4,112 +4,123 @@ import { fileURLToPath } from "url";
 import bcrypt from "bcrypt";
 
 export const getIndex = async (req, res) => {
-  try {
-    return res.status(200).render("example");
-  } catch (error) {
-    return res.status(500).json({
-      message: "Internal server error",
-    });
-  }
+    try {
+        return res.status(200).render("example");
+    } catch (error) {
+        return res.status(500).json({
+            message: "Internal server error",
+        });
+    }
 };
 
 export const postIndex = async (req, res) => {
-  try {
-    return res.status(200).json({
-      message: "postIndex",
-    });
-  } catch (error) {
-    return res.status(500).json({
-      message: "Internal server error",
-    });
-  }
+    try {
+        return res.status(200).json({
+            message: "postIndex",
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: "Internal server error",
+        });
+    }
 };
 
 export const updIndex = async (req, res) => {
-  try {
-    return res.status(200).json({
-      message: "updIndex",
-    });
-  } catch (error) {
-    return res.status(500).json({
-      message: "Internal server error",
-    });
-  }
+    try {
+        return res.status(200).json({
+            message: "updIndex",
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: "Internal server error",
+        });
+    }
 };
 
 export const delIndex = async (req, res) => {
-  try {
-    return res.status(200).json({
-      message: "delIndex",
-    });
-  } catch (error) {
-    return res.status(500).json({
-      message: "Internal server error",
-    });
-  }
+    try {
+        return res.status(200).json({
+            message: "delIndex",
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: "Internal server error",
+        });
+    }
 };
 
 export const getCalendar = async (req, res) => {
-  try {
-    const date = new Date();
+    try {
+        const date = new Date();
 
-    const year = date.getFullYear();
-    const month = date.getMonth();
+        const year = date.getFullYear();
+        const month = date.getMonth();
 
-    const nameMonth = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ];
+        const nameMonth = [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December",
+        ];
 
-    const weekDay = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
+        const weekDay = [
+            "Sunday",
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+        ];
 
-    const positonMonth = nameMonth[date.getMonth()];
+        const positonMonth = nameMonth[date.getMonth()];
 
-    function daysOfTheMonth(currentYear, currentMonth) {
-      const startDate = new Date(currentYear, currentMonth, 1);
-      const endDate = new Date(currentYear, currentMonth + 1, 1);
-      let dates = [];
-      while (startDate < endDate) {
-        dates.push(new Date(startDate));
-        startDate.setDate(startDate.getDate() + 1);
-      }
-      return dates;
+        const [ rows ] = await pool.query(`SELECT * FROM calendar_class ORDER BY CASE
+            WHEN day = 'Sunday' THEN 1
+            WHEN day = 'Monday' THEN 2
+            WHEN day = 'Tuesday' THEN 3
+            WHEN day = 'Wednesday' THEN 4
+            WHEN day = 'Thursday' THEN 5
+            WHEN day = 'Friday' THEN 6
+            WHEN day = 'Saturday' THEN 7
+            END`);
+        console.log(rows)
+
+        //Format hour classes
+        let hourClassesSet = new Set();
+
+        for (let i = 0; i < rows.length; i++) {
+            const temporalHour = parseInt(rows[i].time_start.slice(0, 2));
+            const temporalHourF = parseInt(rows[i].time_finish.slice(0, 2));
+
+            hourClassesSet.add(temporalHour);
+
+            if (temporalHour !== temporalHourF) {
+                hourClassesSet.add(temporalHourF);
+            }
+        }
+
+        let hourClassesArray = Array.from(hourClassesSet).sort((minorHour, higherHour) => minorHour - higherHour);
+        // console.log("hourClassesArray", hourClassesArray);
+
+        return res.status(200).render("calendar", {
+            positonMonth,
+            weekDay,
+            hourClassesArray,
+            rows
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: "Internal server error",
+        });
     }
-
-    const daysMonth = daysOfTheMonth(year, month);
-    const lengthDaysMonth = daysMonth.length;
-    // console.log(lengthDaysMonth);
-    // console.log(daysMonth)
-    // console.log(date)
-    // console.log(nameMonth)
-    return res.status(200).render("calendar", {
-      positonMonth,
-      daysMonth,
-      lengthDaysMonth,
-      weekDay,
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({
-      message: "Internal server error",
-    });
-  }
 };
--- src/server/db/db.sql
+++ src/server/db/db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 07, 2024 at 10:58 PM
+-- Generation Time: Jan 14, 2024 at 01:26 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -32,15 +32,25 @@ CREATE DATABASE acostango;
 USE acostango;
 
 CREATE TABLE `calendar_class` (
-    `id_class` int(11) NOT NULL,
-    `title` varchar(100) NOT NULL,
-    `description` varchar(200) NOT NULL,
-    `date` date DEFAULT NULL,
-    `time_start` time DEFAULT NULL,
-    `time_finish` time DEFAULT NULL,
-    `image` blob DEFAULT NULL
+  `id_calendar` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  `day` varchar(20) NOT NULL,
+  `time_start` time NOT NULL,
+  `time_finish` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- classes rows
+INSERT INTO `calendar_class` (`title`, `day`, `time_start`, `time_finish`) VALUES
+('AcosTango Afterwork Milonga', 'Monday', '19:00:00', '22:00:00'),
+('Beginners', 'Tuesday', '18:00:00', '19:00:00'),
+('Intermediate', 'Tuesday', '19:10:00', '20:30:00'),
+('Tango Strong', 'Tuesday', '19:30:00', '20:45:00'),
+('Practice', 'Wednesday', '20:30:00', '22:00:00'),
+('Newcomers Course', 'Thursday', '19:00:00', '22:00:00'),
+('Special Workshop', 'Saturday', '18:00:00', '19:00:00'),
+('AcosTango Milonga', 'Saturday', '19:00:00', '22:00:00');
+
+INSERT INTO calendar_class(title, day, time_start, time_finish) VALUES ('Beginners', 'Sunday', '20:00:00', '22:00:00');
 -- --------------------------------------------------------
 
 --
@@ -48,43 +58,10 @@ CREATE TABLE `calendar_class` (
 --
 
 CREATE TABLE `gallery` (
-    `id_image` int(11) NOT NULL,
-    `photo` blob DEFAULT NULL
+  `id_image` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `photo` blob DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `calendar_class`
---
-ALTER TABLE `calendar_class`
-    ADD PRIMARY KEY (`id_class`);
-
---
--- Indexes for table `gallery`
---
-ALTER TABLE `gallery`
-    ADD PRIMARY KEY (`id_image`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `calendar_class`
---
-ALTER TABLE `calendar_class`
-    MODIFY `id_class` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `gallery`
---
-ALTER TABLE `gallery`
-    MODIFY `id_image` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;--
