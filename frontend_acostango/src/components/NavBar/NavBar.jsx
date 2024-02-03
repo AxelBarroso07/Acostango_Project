3693b7a89499f06adb9e2122853aa51d6e28402f
IsNorthFace <joseangarher@gmail.com>
2024-02-03 15:34:56 -0300

    add button language and modifications to navbar button, add files to languages

--- frontend_acostango/src/components/NavBar/NavBar.jsx
+++ frontend_acostango/src/components/NavBar/NavBar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import MenuButton from './MenuButton'
+import ButtonLanguage from '../ButtonLanguage/ButtonLanguage'
 
 const Nav = styled.nav`
   height: 150px;
@@ -9,23 +10,6 @@ const Nav = styled.nav`
   align-items: center;
   padding: 20px 50px;
   background-color: #FFFFFF;
-
-  .button{
-    border: none;
-    background-color: #FFFFFF;
-    width: 40px;
-    height: 40px;
-  }
-
-  .button:hover{
-    cursor: pointer;
-  }
-
-  .header__icon{
-    width: 100%;
-    height: 100%;
-    stroke-width:1.5;
-  }
 `
 
 const NavBar = () => {
@@ -33,7 +17,7 @@ const NavBar = () => {
     <Nav>
       <MenuButton />
       <div className="logo"><a href=""><img src="#" alt="acostango_logo" /></a></div>
-      <button className="button"><svg xmlns="http://www.w3.org/2000/svg" className="header__icon" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" /><path d="M3.6 9h16.8" /><path d="M3.6 15h16.8" /><path d="M11.5 3a17 17 0 0 0 0 18" /><path d="M12.5 3a17 17 0 0 1 0 18" /></svg></button>
+      <ButtonLanguage/>
     </Nav>
   )
 }
