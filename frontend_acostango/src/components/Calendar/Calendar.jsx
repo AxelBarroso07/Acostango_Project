374ab0cbe4e00ccda1028498cc3c1a8276957233
ArielBritezDiaz <arielbritezdiaz@gmail.com>
2024-02-02 22:11:44 -0300

    sending base email from finished contact form

--- frontend_acostango/src/components/Calendar/Calendar.jsx
+++ frontend_acostango/src/components/Calendar/Calendar.jsx
@@ -6,63 +6,38 @@ function Calendar() {
   const [ loading, setLoading ] = useState(null)
   const [ error, setError ] = useState(null)
 
-  const fetchDataCalendar = useCallback(async () => {
-    try {
-      const HOST = import.meta.env.VITE_DB_HOST;
-      const PORT = import.meta.env.VITE_PORT_SERVER;
+  // const fetchDataCalendar = useCallback(async () => {
+  //   try {
+  //     const HOST = import.meta.env.VITE_DB_HOST;
+  //     const PORT = import.meta.env.VITE_PORT_SERVER;
 
-      const response = await fetch(`http://${HOST}:${PORT}/calendar`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
+  //     const response = await fetch(`http://${HOST}:${PORT}/calendar`, {
+  //       method: 'GET',
+  //       headers: {
+  //         'Content-Type': 'application/json'
+  //       }
+  //     });
 
-      const result = await response.json();
-      console.log("Data calendar:", result);
+  //     const result = await response.json();
+  //     console.log("Data calendar:", result);
 
-      const fetchedData = result.data
+  //     const fetchedData = result.data
 
-      setData(fetchedData);
-    } catch (error) {
-      setError(error.message);
-    } finally {
-      setLoading(false);
-    }
-  }, []);
+  //     setData(fetchedData);
+  //   } catch (error) {
+  //     setError(error.message);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // }, []);
 
-  useEffect(() => {
-    fetchDataCalendar();
-  }, [fetchDataCalendar]);
-
-  if(loading) {
-    return <p>Cargando</p>;
-  }
-
-  if(error) {
-    return <p>Error: {error}</p>
-  }
+  // useEffect(() => {
+  //   fetchDataCalendar();
+  // }, [fetchDataCalendar]);
 
   return (
     <section className='calendar'>
       <h1 className='calendar__title'>CALENDAR</h1>
-      {data && data.length > 0 ? (
-        <ul>
-          {data.map(item => (
-            <li key={item.idCalendar}>
-              <p>{item.idCalendar}</p>
-              <p>{item.title}</p>
-              <p>{item.description}</p>
-              <p>{item.day}</p>
-              <p>{item.time12hrsStartFormat}</p>
-              <p>{item.time12hrsStartFormat}</p>
-              <p>{item.workshop}</p>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>No hay datos disponibles</p>
-      )}
     </section>
   )
 }
