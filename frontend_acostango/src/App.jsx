edf99e46d17be18a2ac8552f1c18b6e3f892ab6c
IsNorthFace <joseangarher@gmail.com>
2024-02-06 15:17:52 -0300

    fixed urls

--- frontend_acostango/src/App.jsx
+++ frontend_acostango/src/App.jsx
@@ -6,27 +6,6 @@ import PageMilonga from "./Pages/PageMilonga";
 import PageEvents from "./Pages/PageEvents";
 import PageRent from "./Pages/PageRent";
 import Error404 from "./Pages/Error404";
-import i18n from "i18next";
-import { useTranslation, initReactI18next } from "react-i18next";
-
-i18n
-  .use(initReactI18next)
-  .init({
-    resources: {
-      en: {
-        translation: {
-          "Welcome to React": "Welcome to React and react-i18next"
-        }
-      },
-      ger: {
-        translation: {
-          "Welcome to React": "Willkommen bei React und react-i18next"
-        }
-      }
-    },
-    lng: "en",
-    fallbackLng: "en",
-  });
 
 function App() {
   return (
