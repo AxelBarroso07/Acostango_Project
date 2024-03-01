c5643516ede58b80ac01d95e276e457fb1d56367
MarianoJSB <mariano.n.aguiar@gmail.com>
2024-02-29 22:37:05 -0300

    marker icon map in production

--- Acostango/src/components/MapView/MapView.jsx
+++ Acostango/src/components/MapView/MapView.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css'
 import '../MapView/MapView.css'
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 
 export const MapView = () => {
+    let DefaultIcon = L.icon({
+        iconUrl: icon,
+        shadowUrl: iconShadow
+    });
+    
+    L.Marker.prototype.options.icon = DefaultIcon;    
     return (
         <div>
             <MapContainer center={[48.190493718277075, 16.353333125430908]} zoom={17} scrollWheelZoom={true} className='mapa'>
