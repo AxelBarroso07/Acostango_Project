b2cb04213cbf4cc85cbd4083f9a2c71cd079fa46
Mauro <mauisrock@hotmail.com>
2024-03-02 02:58:59 +0000

    chore: add config prod

--- Acostango/db.js
+++ Acostango/db.js
@@ -2,6 +2,9 @@ import { createPool } from 'mysql2/promise';
 
 import { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_DATABASE } from './config.js';
 
+
+console.log('DATABASE INFO: ', {DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_DATABASE})
+
 export const pool = createPool({
     host: DB_HOST,
     user: DB_USER,
