9fe908259bc764ae213f04f2884761ad73379516
Mauro <mauisrock@hotmail.com>
2024-03-02 00:43:23 -0300

    chore: add domain in .emv

--- Administrator/config.js
+++ Administrator/config.js
@@ -5,6 +5,7 @@ dotenv.config();
 // export the env variables
 export const PORT = process.env.PORT;
 export const DOMAIN_ADMIN = process.env.DOMAIN_ADMIN;
+export const DB_HOST = process.env.DB_HOST;
 export const DB_PORT = process.env.DB_PORT;
 export const DB_USER = process.env.DB_USER;
 export const DB_PASSWORD = process.env.DB_PASSWORD;
